42d8511b1ab5fdb0ca2d2047333b80b8aa521949
John Lim <johnlimed@gmail.com>
2016-09-20 21:53:19 +0800

    Now able to split each individual word and tag

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # pos_tagger
+##Data files
+Data files must be placed in tagger/a2_data directory.
+- sents.devt
+- sents.out
+- sents.test
+- sents.train
+
+Model output file will be generated in tagger/a2_data directory named model_file.
+
+##Running tagger
+Two commands, either build_tagger or test_tagger
+
+cmd to build:
+python build_tagger sents.train sents.devt model_file
+
+cmd to test:
+python run_tagger sents.test model_file sents.out
--- tagger/build_tag/__init__.py
+++ tagger/build_tag/__init__.py
@@ -1,10 +1,35 @@
 import os.path
+import pprint
+import json
 DATA_DIR = "a2_data\\"
-
+pp = pprint.PrettyPrinter()
 
 def train_tagger():
+    __count_words()
+
+
+def __count_words():
+    total_word_count = 0
+    word_type_count = dict()
     training_set = open(os.path.join(os.getcwd(), DATA_DIR + 'sents.train'))
     for line in training_set:
         line_list = line.split(" ")
-        for word in line_list:
-            print word
+        for word_with_tag in line_list:
+            word, tag = word_with_tag.rstrip().rsplit('/', 1)
+            total_word_count += 1
+            # print word, tag, total_word_count
+            if tag in word_type_count:
+                if word in word_type_count[tag]:
+                    word_type_count[tag][word] += 1
+                else:
+                    total_word_count += 1
+                    word_type_count[tag][word] = 1
+            else:
+                word_type_count[tag] = {word: 1}
+    pp.pprint(word_type_count)
+    with open('result.json', 'w') as outfile:
+        json.dump(word_type_count, outfile)
+
+def __count_cond_words():
+    total_word_count = 0
+    cond_word_types = dict()
