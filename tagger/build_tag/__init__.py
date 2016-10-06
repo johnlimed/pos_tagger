89dc0c66fb1c6d38cc843fc133ed44d959411922
John Lim <johnlimed@gmail.com>
2016-10-06 23:22:09 +0800

    Calculating probability...

--- tagger/build_tag/__init__.py
+++ tagger/build_tag/__init__.py
@@ -13,7 +13,7 @@ COUNT_PREV_TAG_V_TAG = "count_prev_tag_v_tag"
 COUNT_TAG = "count_total_tag"
 COUNT_T_V_W = "count_t_v_w"
 COUNT_W_V_T = "count_w_v_t"
-PROB_PREV_TAG_V_TAG = "prob_tag_v_prev_tag"
+PROB_TAG_V_PREV_TAG = "prob_tag_v_prev_tag"
 PROB_W_V_T = "prob_w_v_t"
 pp = pprint.PrettyPrinter()
 
@@ -27,14 +27,14 @@ def train_tagger(training_file, devt_file, out_file):
         # COUNT_W_V_T: count_w_v_t,
         # COUNT_PREV_TAG_V_TAG: count_prev_t_v_t,
         COUNT_TAG: count_tag,
-        PROB_PREV_TAG_V_TAG: prob_t_given_prev_t,
+        PROB_TAG_V_PREV_TAG: prob_t_given_prev_t,
         PROB_W_V_T: prob_w_given_t
     }
     utils.write_out_file(out_dict, out_file)
 
 
 def __count_bi_word(training_file):
-    total_word_count, count_previous_t_against_t, count_tag_against_word, count_word_against_tag = 0, {}, {}, {}
+    total_word_count, count_t_against_previous_t, count_tag_against_word, count_word_against_tag = 0, {}, {}, {}
     count_tag = {}
     training_set = open(training_file)
     for line in training_set:
@@ -46,10 +46,10 @@ def __count_bi_word(training_file):
         for idx, word_w_tag in enumerate(line_list):
             if idx == 0:
                 no_pass = False
-                if SPECIAL_START in count_tag:
-                    count_tag[SPECIAL_START] += 1
-                else:
-                    count_tag[SPECIAL_START] = 1
+                # if SPECIAL_START in count_tag:
+                #     count_tag[SPECIAL_START] += 1
+                # else:
+                #     count_tag[SPECIAL_START] = 1
             elif idx == 1:
                 no_pass = True
                 word, tag = word_w_tag.rsplit('/', 1)
@@ -61,29 +61,29 @@ def __count_bi_word(training_file):
                 previous_tag = line_list[idx - 1].rsplit('/', 1)[1]
                 tag = SPECIAL_END
                 word = previous_tag
-            __count_previous_t_against_t(count_previous_t_against_t, tag, previous_tag, no_pass)
+            __count_t_against_previous_t(count_t_against_previous_t, tag, previous_tag, no_pass)
             __count_tag_against_word(count_tag_against_word, word, tag, count_tag, no_pass)
             __count_word_against_tag(count_word_against_tag, word, tag, no_pass)
             # print idx, word, tag, previous_tag
             # break
     # pp.pprint(count_tag_against_word)
-    # pp.pprint(count_previous_t_against_t)
+    # pp.pprint(count_t_against_previous_t)
     utils.write_out_file(count_tag_against_word, TAG_WORD_COUNT_FILENAME)
     utils.write_out_file(count_word_against_tag, WORD_TAG_COUNT_FILENAME)
-    utils.write_out_file(count_previous_t_against_t, PREV_TAG_V_TAG_COUNT_FILENAME)
+    utils.write_out_file(count_t_against_previous_t, PREV_TAG_V_TAG_COUNT_FILENAME)
     utils.write_out_file(count_tag, COUNT_TAG_FILENAME)
-    return count_tag_against_word, count_word_against_tag, count_previous_t_against_t, count_tag
+    return count_tag_against_word, count_word_against_tag, count_t_against_previous_t, count_tag
 
 
-def __count_previous_t_against_t(count_previous_t_against_t, tag, previous_tag, no_pass):
+def __count_t_against_previous_t(count_t_against_previous_t, tag, previous_tag, no_pass):
     if no_pass:
-        if previous_tag in count_previous_t_against_t:
-            if tag in count_previous_t_against_t[previous_tag]:
-                count_previous_t_against_t[previous_tag][tag] += 1
+        if tag in count_t_against_previous_t:
+            if previous_tag in count_t_against_previous_t[tag]:
+                count_t_against_previous_t[tag][previous_tag] += 1
             else:
-                count_previous_t_against_t[previous_tag][tag] = 1
+                count_t_against_previous_t[tag][previous_tag] = 1
         else:
-            count_previous_t_against_t[previous_tag] = {tag: 1}
+            count_t_against_previous_t[tag] = {previous_tag: 1}
 
 
 def __count_tag_against_word(count_tag_against_word, word, tag, count_tag, no_pass):
@@ -122,17 +122,17 @@ def __count_cond_probabilities(count_t_v_w, count_w_v_t, count_prev_t_v_t, count
             if tag in count_w_v_t[word]:
                 prob_w_given_t[word][tag] = count_w_v_t[word][tag] / float(count_tag[tag])
             else:
-                prob_w_given_t[word][tag] = 0
-        for prev_tag in count_prev_t_v_t:
-            if prev_tag not in prob_t_given_prev_t:
-                prob_t_given_prev_t[prev_tag] = {}
-            if tag in count_prev_t_v_t[prev_tag].keys():
-                prob_t_given_prev_t[prev_tag][tag] = count_prev_t_v_t[prev_tag][tag] / float(count_tag[prev_tag])
+                prob_w_given_t[word][tag] = 0.000087
+        for cur_tag in count_prev_t_v_t:
+            if cur_tag not in prob_t_given_prev_t:
+                prob_t_given_prev_t[cur_tag] = {}
+            if tag in count_prev_t_v_t[cur_tag].keys():
+                prob_t_given_prev_t[cur_tag][tag] = count_prev_t_v_t[cur_tag][tag] / float(count_tag[cur_tag])
             else:
-                prob_t_given_prev_t[prev_tag][tag] = 0
+                prob_t_given_prev_t[cur_tag][tag] = 0.000087
 
-            # for tag in count_prev_t_v_t[prev_tag]:
-            #     prob_t_given_prev_t[prev_tag][tag] = count_prev_t_v_t[prev_tag][tag] / float(count_tag[prev_tag])
+            # for tag in count_prev_t_v_t[cur_tag]:
+            #     prob_t_given_prev_t[cur_tag][tag] = count_prev_t_v_t[cur_tag][tag] / float(count_tag[cur_tag])
 
     # __cal_unigram(count_tag, count_w_v_t, prob_w_given_t)
     # for word in count_w_v_t:
--- tagger/test_tag/__init__.py
+++ tagger/test_tag/__init__.py
@@ -13,6 +13,7 @@ SPECIAL_START = "<S>"
 SPECIAL_END = "/<S>"
 pp = pprint.PrettyPrinter()
 
+
 # stats_dict = {
 #     PROB_PREV_TAG_V_TAG: prob_t_given_prev_t,
 #     PROB_W_V_T: prob_w_given_t
@@ -30,16 +31,32 @@ def test_tagger(test_file, in_filename, out_file):
         word_list = line.rstrip().split(" ")
         word_list.insert(0, SPECIAL_START)
         word_list.append(SPECIAL_END)
-        viterbi = {0: [1], 1: []}
-        backpointer = {}
-        for tags in pos_tags[1:-1]:
-            viterbi[1].append(math.log1p(prob_w_v_t[word_list[1]][tags]))
-            backpointer[tags] = {1: 0}
-            # print viterbi[1]
+        viterbi = {0: [(1, SPECIAL_START)], 1: []}
+        backpointer = {"start"}
+        backpointer.add(SPECIAL_START)
+        for tag in pos_tags[1:-1]:
+            viterbi[1].append((math.log1p(prob_w_v_t[word_list[1]][tag]), tag))
+        for idx, word in enumerate(word_list[2:]):
+            viterbi[idx + 2] = []
+            for tag in pos_tags[:-1]:
+                # print "we are at: " + str(idx+2)
+                # print "and tag: " + tag + " and word " + word
+                if word in prob_w_v_t:
+                    # print "prob w_v_t: " + str(prob_w_v_t[word][tag])
+                    viterbi[idx + 2].append(__det_tag_prob(viterbi[idx + 1], prob_t_v_t[tag], prob_w_v_t[word][tag],
+                                                           backpointer))
+                else:
+                    print "FAILLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL"
+                    # TODO: make unknown words work
+                    break
+                    # break
+                    # break
+                    # print viterbi[idx+2]
+        # break
         pprint.pprint(viterbi)
-        # for idx, word in enumerate(word_list[2:]):
-        #     print word
-
+        print backpointer
+        # viterbi[idx].append()
+        # print word, idx
 
 
         # for idx, word in enumerate(word_list[:-1]):
@@ -62,7 +79,22 @@ def test_tagger(test_file, in_filename, out_file):
         #             __determine_unknown_prob(word)
 
 
+def __det_tag_prob(list_of_prev_prob, list_prob_t_v_t, prob_w_given_tag, backpointer):
+    # TODO: If prev prob tag is UNKN, determine most likely tag
+    max_prob = (0, "NN")
+    # max_prob = (float("-Infinity"), "")
+    # print list_prob_t_v_t
+    for tuple_prob in list_of_prev_prob:
+        # print "tuple prob: " + str(tuple_prob)
+        prob = math.log1p(tuple_prob[0]) + math.log1p(list_prob_t_v_t[tuple_prob[1]] + math.log1p(prob_w_given_tag))
+        if prob > max_prob[0]:
+            max_prob = (prob, tuple_prob[1])
+            backpointer.add(tuple_prob[1])
+    # print "Max prob: " + str(max_prob[0])
+    return max_prob
+
+
 def __determine_unknown_prob(unknown_word):
     print "unknown word... " + unknown_word
-    prob = 0*unknown_word
+    prob = 0 * unknown_word
     return prob
